2e089ba9202021faa4654e4b818b9c5165564bcb
Abhishek Sharma <abhi.sharma1177@gmail.com>
2021-06-17 00:52:35 +0530

    shopping-bag

--- cosmo/src/app/shopping-bag/shopping-bag.component.ts
+++ cosmo/src/app/shopping-bag/shopping-bag.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-bag',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingBagComponent implements OnInit {
 
-  constructor() { }
+  public rewardPoints: number = 646;
+  public reward:boolean = false;
+  public bagTotal: number = 6150;
+  public grandTotal: number = this.bagTotal;
+  private sustainable: boolean = true;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onChange(event){
+    this.reward = event.target.checked;
+    this.grandTotal = (this.reward)? this.bagTotal - Math.floor(this.rewardPoints/100): this.bagTotal;
+  }
+
+  proceed() {
+    this.router.navigate(['checkout']);
+  }
+
+  onChangeSustainable(event){
+    this.sustainable = event.target.checked;
   }
 
 }
