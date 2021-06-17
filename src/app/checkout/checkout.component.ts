97dcbabbd7b9ca1bacc28f59eef1a2541be0c5e7
Abhishek Sharma <abhi.sharma1177@gmail.com>
2021-06-17 19:30:05 +0530

    code taken out of cosmo

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
+  public rewardPoints: number = 646;
+  public reward:boolean = false;
+  public bagTotal: number = 6150;
+  public grandTotal: number = this.bagTotal-Math.floor(this.rewardPoints/100);
   constructor() { }
 
   ngOnInit(): void {
